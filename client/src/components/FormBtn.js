80804e32c75702b061aff5c125558c10f1fb8257
Steven Polido <steven.polido@vistage.com>
2020-07-29 14:09:22 -0700

    Updated styled component form button styles

--- client/src/components/FormBtn.js
+++ client/src/components/FormBtn.js
@@ -8,11 +8,17 @@ export default class FormBtn extends PureComponent {
             padding: 0.8rem 2rem;
             color: #fff;
             width: 100%;
-            border: none;
+            border: solid 1px #000;
             margin: 1rem 0;
             cursor: pointer;
             outline: none;
             border-radius: 3px;
+            transition: .12s ease-in-out;
+            &:hover {
+                background: #fff;
+                border: solid 1px #000;
+                color: #000;
+            }
         `;
         return <Button onClick={this.props.handleSignIn}>{ this.props.btnText }</Button>
     }
