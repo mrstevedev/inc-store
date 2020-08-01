9ca6245fa56e3de01061d267c067cbc749554b9c
Steven Polido <steven.polido@vistage.com>
2020-07-31 20:39:23 -0700

    Refactored LocalStrategy params

---
+++ config/passport.js
@@ -0,0 +1,34 @@
+const User = require('../models/User');
+const bcrypt = require("bcrypt");
+const LocalStrategy = require("passport-local").Strategy;
+
+module.exports = function (passport) {
+  passport.use(
+    new LocalStrategy((username, password, done) => {
+        User.findOne({ username, username }, (err, user) => {
+            if(err) throw err;
+            if(!user) return done(null, false)
+            bcrypt.compare(password, user.password, (err, result) => {
+                if(err) throw err;
+                if(result === true) {
+                    return done(null, user)
+                } else {
+                    return done(null, false)
+                }
+            })
+        })
+    })
+  );
+
+  // Serialize User
+  passport.serializeUser(function (user, done) {
+    done(null, user.id);
+  });
+
+  // Deserialize User
+  passport.deserializeUser(function (id, done) {
+    User.findById( { _id: id }, function (err, user) {
+      done(err, user);
+    });
+  });
+};
