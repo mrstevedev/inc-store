ca4078a0ee879bc503a47937369022c93bbc443f
Steven Polido <steven.polido@vistage.com>
2020-08-03 18:55:56 -0700

    Added passport, express-session, and cookie-parser middlewares.

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser')
 const helmet = require('helmet');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const connectDB = require('./config/db');
 const morgan = require('morgan');
+const passport = require('passport');
+const session = require('express-session');
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
@@ -18,11 +21,27 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}));
+
+app.use(cookieParser('secret'))
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+// Passport config 
+require('./config/passport')(passport);
+
 app.use(morgan('dev'));
 
 app.use(helmet());
 
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:5001',
+    credentials: true
+}));
 
 app.use(express.static('public'));
 
@@ -30,7 +49,7 @@ app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
-app.use('/signin', require('./routes/api/users/signin'));
+app.use('/signin', require('./routes/signin'));
 app.use('/api/users', require('./routes/api/users/signup'));
 
 const PORT = process.env.PORT || 5000;
