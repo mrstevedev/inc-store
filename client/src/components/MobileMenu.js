0abc5f12d511a16208baafc222ac655dc72ab0d2
Steven Polido <stevendotpulido@gmail.com>
2021-04-07 18:31:29 -0700

    Removed component in place of jsx unordered list

--- client/src/components/MobileMenu.js
+++ client/src/components/MobileMenu.js
@@ -1,46 +1,79 @@
 import React, { Component, Fragment } from "react";
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 export class MobileMenu extends Component {
+
+
+    handleShowSignInPage(event) {
+        console.log('show signin page');
+        const mobileMenu = document.querySelector('.mobile-menu');
+
+        mobileMenu.classList.toggle('show');
+
+        // add checked to input
+        const MobileToggle = document.querySelector('.menu-icon__checkbox').checked = false;
+        
+    }
+
   render() {
     const MobileMenu = styled.div`
-    display: none;
-    @media(max-width: 415px) {
-      background: transparent;
-      position: absolute;
-      z-index: 2;
-      width: 100%;
-      height: 100%;
-      left: 0;
-      top: 5.6rem;
+    @media(max-width: 465px) {
+        position: absolute;
+        z-index: 2;
+        width: 100%;
+        height: 100%;
+        left: -400px;
+        top: 5.6rem;
+        margin: -50px 0 0 0;
+        padding-top: 125px;
+        background-color: #fff;
+        z-index: 1;
+        -webkit-transition: 400ms ease;
+        transition: 400ms ease;
+    }`;
 
-      margin: -50px 0 0 0;
-      padding-top: 125px;
-      background-color: #F5F6FA;
-      -webkit-font-smoothing: antialiased;
-      transform-origin: 0% 0%;
-      transform: translate(0%, 0);
-      transition: transform 0.5s cubic-bezier(0.77,0.2,0.05,1.0);
-    }
-       
+    const SignInContainer = styled.div`
+        margin: 3rem 0;
     `;
-    const SlideMenu = styled.div`
-    //   height: 100%;
-    //   background: #fff;
-    //   width: 80%;
+
+    const AccountList = styled.ul`
+        list-style: none;
+        padding: 0;
+        li {
+            a {
+                text-transform: uppercase;
+                font-weight: bold;
+                // color: #008cfb;
+
+                // &:after {
+                //     content: "";
+                //     display: block;
+                //     border: solid 1px;
+                //     width: 28px;
+                //     margin: 3px 0;
+                // }
+            }
+        }
     `;
+
     return (
       <Fragment>
-        <MobileMenu className="mobile-menu">
-          <SlideMenu>
+        <MobileMenu className="mobile-menu">            
             <ul id="menu">
                 <li><a href="#">Sale</a></li>
                 <li><a href="#">Women</a></li>
                 <li><a href="#">Men</a></li>
                 <li><a href="#">Beauty</a></li>
                 <li><a href="#">Brands</a></li>
+
+                <SignInContainer>
+                    <AccountList>
+                        <li><Link to="/dashboard">Eckosneekz</Link> | <Link className="inc__signin-link" to="/signin" onClick={this.handleShowSignInPage}>Sign In</Link></li>
+                    </AccountList>
+                </SignInContainer>
             </ul>
-          </SlideMenu>
+            
         </MobileMenu>
       </Fragment>
     );
